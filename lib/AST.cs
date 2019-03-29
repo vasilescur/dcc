c3de0e3b867b08e9d354d1f7c74f8b829f30e80b
Radu Vasilescu <vasilescur@gmail.com>
2019-03-29 18:48:46 -0400

    Created AST structure, working on Parser

---
+++ lib/AST.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DCC {
+    abstract class Expression { }
+
+        abstract class Operation : Expression {
+            public OperationType operationType;
+
+            public enum OperationType {
+                Addition, Subtraction,
+                TestEq, TestGt, TestLt, TestGeq, TestLeq,
+                Increment, Decrement,
+                Not, Xor, ShiftLeft, ShiftRight,
+                Dereference, AddressOf
+            }
+        }
+
+            class SingleParamOperation : Operation {
+                public Expression operand;
+            }
+
+            class TwoParamOperation : Operation {
+                public List<Expression> operands;
+            }
+
+        class LiteralConstant : Expression {
+            public int value;
+        }
+
+        class Variable : Expression {
+            public string name;
+        }
+
+            class Pointer : Variable { }
+
+    abstract class Action { }
+
+        class Instruction : Action {
+            public Instruction.Type instructionType;
+            public List<Expression> parameters;
+
+            public enum Type {
+                Assignment, VarDeclare, FuncCall, Return, Assembly
+            }
+        }
+
+        abstract class ControlFlowUnit : Action { }
+
+            class If : ControlFlowUnit {
+                public Expression condition;
+                public List<Action> ifActions;
+            }
+
+                class IfElse : If {
+                    public List<Action> elseActions;
+                }
+
+            class While : ControlFlowUnit {
+                public Expression condition;
+                public List<Action> actions;
+            }
+
+            class For : ControlFlowUnit {
+                public Action forSetupAction;
+                public Expression forCondition;
+                public Action forIncrementAction;
+            }
+
+    class Function {
+        public string name;
+        public Type returnsValue;
+        public List<Variable> arguments;
+        public List<Action> actions;
+    }
+
+    class AbstractProgram {
+        public List<Function> functions;
+        public List<Variable> globalVars;
+    }
+}
--- lib/Parser.cs
+++ lib/Parser.cs
@@ -6,3 +6,56 @@
 ///<date>2019-03-29</date>
 
 using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DCC {
+    class Parser {
+        public AbstractProgram program;
+        public List<Token> source;
+
+        public Parser(List<Token> source) {
+            this.source = source;
+        }
+
+        public void Parse() {
+            string context = "global";
+            string subcontext = "";
+            int exprLevel = 0;
+
+            string temp_type = "";
+            string temp_name = "";
+
+            foreach (Token token in source) {
+                switch (context) {
+                    case "global": {
+                        // First token *has* to be a type name
+                        
+                    } break;
+
+                    case "function": {
+                        switch (subcontext) {
+                            case "args": {
+
+                            } break;
+
+                            case "": {      // General function instructions
+
+                            } break;
+
+                            default: {
+                                throw new Exception("Unknown Context: " + context + "/" + subcontext);
+                            }
+                        }
+                    } break;
+
+                    default: {
+                        throw new Exception("Unknown Context: " + context);
+                    }
+                }
+            }
+        }
+    }
+}
